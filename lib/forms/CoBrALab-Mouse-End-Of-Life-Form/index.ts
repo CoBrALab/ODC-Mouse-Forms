2821991c1d29bc83e55ef6a951141931367838d1
david-roper <davidroper321@gmail.com>
2024-07-22 16:15:32 -0400

    added body extraction section

--- lib/forms/CoBrALab-Mouse-End-Of-Life-Form/index.ts
+++ lib/forms/CoBrALab-Mouse-End-Of-Life-Form/index.ts
@@ -3,6 +3,19 @@
 const { defineInstrument } = await import('/runtime/v1/opendatacapture@1.0.0/core.js');
 const { z } = await import('/runtime/v1/zod@3.23.6/index.js');
 
+function createDependentField<T>(field: T, fn: (bodyExtractionDone: boolean) => boolean) {
+    return {
+      kind: 'dynamic' as const,
+      deps: ["bodyExtractionDone"] as const,
+      render: (data: { bodyExtractionDone: boolean }) => {
+        if (fn(data.bodyExtractionDone)) {
+          return field;
+        }
+        return null;
+      }
+    };
+}
+
 export default defineInstrument({
   kind: 'FORM',
   language: 'en',
@@ -12,19 +25,65 @@ export default defineInstrument({
     name: 'MOUSE_END_OF_LIFE_FORM'
   },
   content: {
-    terminationKind: {
+    terminationReason: {
       kind: 'string',
-      variant: 'radio',
+      variant: 'select',
       label: "Reason for termination",
       options: {
         "End of Experiment":"End of Experiment",
         "Humane endpoint": "Humane Endpoint",
-        "Vetinary Endpoint": "Vetinary Endpoint",
+        "Veterinary Endpoint": "Veterinary Endpoint",
         "Surplus":"Surplus",
         "Surgical complications":"Surgical complications",
       }
-      
-    }
+    },
+    terminationComments: {
+     kind: 'dynamic',
+     deps: ["terminationReason"],
+     render(data) {
+        if(data.terminationReason === "Humane endpoint" || data.terminationReason === "Surplus" ){
+          return {
+            kind: 'string',
+            variant: "textarea",
+            label: "Comments"
+          }
+        }
+        return null
+     }
+    },
+    terminationType: {
+      kind: "string",
+      variant: 'select',
+      label: 'Form of termination',
+      options: {
+        "Gas induction":"Gas induction",
+        "Perfusion": "Perfusion",
+        "Guillotine": "Guillotine",
+        "Cardiac puncture":"Cardiac puncture",
+        "Cervical dislocation": "Cervical dislocation"
+      }
+    },
+    bodyExtractionDone: {
+      kind: 'boolean',
+      variant: "radio",
+      label: "Body part extracted"
+    },
+    bodyPartExtracted: createDependentField({
+      kind: 'set',
+      variant: "listbox",
+      label: "Body part extracted",
+      options: {
+        "Brain":"Brain",
+        "Gut": "Gut",
+        "Fat tissue": "Fat tissue",
+        "Heart":"Heart"
+    },
+    }, (type) => type === true),
+    bodyExtractionReason: createDependentField({
+      kind: 'string',
+      variant: "textarea",
+      label: "Reason for Extraction"
+    }, (type) => type === true)
   },
   details: {
     description: 'Form to fill in info of mouse end of life',
@@ -35,6 +94,11 @@ export default defineInstrument({
   },
   measures: {},
   validationSchema: z.object({
-    terminationKind: z.string()
+    terminationReason: z.string(),
+    terminationComments: z.string(),
+    terminationType: z.string(),
+    bodyExtractionDone: z.boolean(),
+    bodyPartExtracted: z.set(z.enum(["Brain","Gut", "Heart", "Fat tissue"])),
+    bodyExtractionReason: z.string()
   })
 });
