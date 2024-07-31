aa0b092768119443225bdb6ece51800f689f7b3c
David Roper <davidroper321@gmail.com>
2024-07-31 16:37:18 -0400

    added content to housing form

---
+++ lib/forms/CoBrALab-Mouse-Housing-Form/index.ts
@@ -0,0 +1,113 @@
+/* eslint-disable perfectionist/sort-objects */
+
+const { defineInstrument } = await import('/runtime/v1/opendatacapture@1.0.0/core.js');
+const { z } = await import('/runtime/v1/zod@3.23.6/index.js');
+
+export default defineInstrument({
+  kind: 'FORM',
+  language: 'en',
+  tags: ['Housing','Enrichment','Room Change'],
+  internal: {
+    edition: 1,
+    name: 'MOUSE_HOUSING_FORM'
+  },
+  content: {
+    roomNumber: {
+      kind: 'string',
+      variant: "input",
+      label: "Room number"
+    },
+    totalMice: {
+      kind: "number",
+      variant: "input",
+      label: "number of mice in cage"
+    },
+    cageNumber: {
+      kind: "number",
+      variant: "input",
+      label: "Cage number"
+    },
+    beddingType: {
+      kind: 'string',
+      variant: "radio",
+      label: "Bedding type",
+      options: {
+        "Corncob": "Corncob",
+        "Woodchip": "Woodchip"
+      }
+    },
+    cageChangeDay: {
+      kind: "string",
+      variant: "select",
+      label: "Cage change day",
+      options: {
+        "Monday": "Monday",
+        "Tuesday": "Tuesday",
+        "Wednesday": "Wednesday",
+        "Thursday": "Thursday",
+        "Friday": "Friday",
+        "Saturday": "Saturday",
+        "Sunday": "Sunday"
+      }
+    },
+    bottleType: {
+      kind: "string",
+      variant: "radio",
+      label: "Bottle type",
+      options: {
+        "Auto Bottle": "Auto Bottle",
+        "Standard": "Standard"
+      }
+    },
+    cageType: {
+      kind: "string",
+      variant: "radio",
+      label: "Cage type",
+      options: {
+        "Enriched": "Enriched",
+        "Standard": "Standard"
+      }
+    },
+    cageEnrichment: {
+      kind: "dynamic",
+      deps: ["cageType"],
+      render(data) {
+        if(data.cageType === 'Enriched'){
+           return {
+            kind: "set",
+            variant: "listbox",
+            label: "Forms of enrichment",
+            options: {
+              "Wheel":"Wheel",
+              "Cover": "Cover",
+              "Tube": "Tube",
+              "Enlarged Cage": "Enlarged Cage"
+            }
+           }
+        }
+        return null
+      }
+    }
+  },
+  details: {
+    description: 'Form to describe a mouses housing',
+    estimatedDuration: 1,
+    instructions: ['Please describe the mouses housing whenever it is changed'],
+    license: 'UNLICENSED',
+    title: 'Mouse Housing Form'
+  },
+  measures: {
+    
+  },
+  validationSchema: z.object({
+    roomNumber: z.string(),
+    totalMice: z.number(),
+    cageNumber: z.number(),
+    beddingType: z.string(),
+    cageChangeDay: z.string(),
+    bottleType: z.string(),
+    cageType: z.string(),
+    cageEnrichment: z.set(z.enum(['Wheel',"Cover","Tube","Enlarged Cage"])).optional(),
+
+  })
+});
