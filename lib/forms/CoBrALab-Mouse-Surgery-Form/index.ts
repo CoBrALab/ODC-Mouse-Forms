8f3953cf4d056beb6e06f4d49173aed3b3eb4c0e
David Roper <davidroper321@gmail.com>
2024-08-07 16:53:49 -0400

    added tags, title, desc and intructions

--- lib/forms/CoBrALab-Mouse-Surgery-Form/index.ts
+++ lib/forms/CoBrALab-Mouse-Surgery-Form/index.ts
@@ -6,18 +6,18 @@ const { z } = await import('/runtime/v1/zod@3.23.6/index.js');
 export default defineInstrument({
   kind: 'FORM',
   language: 'en',
-  tags: ['<PLACEHOLDER>'],
+  tags: ['Surgery', 'Vet Care', 'Wound Treatment', 'Ovariectomy'],
   internal: {
     edition: 1,
-    name: '<PLACEHOLDER>'
+    name: 'MOUSE_SURGERY_FORM'
   },
   content: {},
   details: {
-    description: '<PLACEHOLDER>',
+    description: 'Form to be filled in when a mouse goes through a surgery or veterinary care',
     estimatedDuration: 1,
-    instructions: ['<PLACEHOLDER>'],
+    instructions: ['Please fill this form whenever a mouse has to through a surgery or has gone through veterinary care'],
     license: 'UNLICENSED',
-    title: '<PLACEHOLDER>'
+    title: 'Mouse surgery, wound treatment and veterinary care form'
   },
   measures: {},
   validationSchema: z.object({})
