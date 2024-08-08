4e61730b5132f1f6a9dc63e2cc05646574a33840
David Roper <davidroper321@gmail.com>
2024-08-08 11:25:57 -0400

    add wound treatment question and measures section

--- lib/forms/CoBrALab-Mouse-Surgery-Form/index.ts
+++ lib/forms/CoBrALab-Mouse-Surgery-Form/index.ts
@@ -31,14 +31,149 @@ export default defineInstrument({
       label: "Select physical intervention",
       options: {
         "Surgery": "Surgery",
-        "Wound Treatment": "Wound Treatment"
+        "Wound treatment": "Wound treatment"
       }
     },
-    analglesiaUsed: createDependentField({
+    aneglesiaUsed: createDependentField({
       kind: "boolean",
       variant: "radio",
-      label: "Analglesia used"
-    }, (type) => type === "Surgery")
+      label: "Aneglesia used"
+    }, (type) => type === "Surgery"),
+
+    aneglesiaType: {
+      kind: "dynamic",
+      deps: ["aneglesiaUsed"],
+      render(data) {
+        if(data.aneglesiaUsed){
+          return {
+            kind: "string",
+            variant: "input",
+            label: "Aneglesia type"
+          }
+        }
+        return null
+      }
+    },
+    aneglesiaVolume: {
+       kind: "dynamic",
+       deps: ["aneglesiaUsed"],
+       render(data) {
+        if(data.aneglesiaUsed){
+          return {
+          kind: "string",
+          variant: "input",
+          label: "Aneglesia volume"
+          }
+        }
+        return null
+      }
+      
+    },
+    stereotaxUsed: createDependentField({
+      kind: "boolean",
+      variant: "radio",
+      label: "Stereotax used"
+    },(type) => type === "Surgery"),
+
+    stereotaxId:{
+      kind: "dynamic",
+      deps: ["stereotaxUsed"],
+      render(data) {
+        if(data.stereotaxUsed){
+          return {
+            kind: "string",
+            variant: "input",
+            label: "Stereotax ID"
+          }
+        }
+        return null
+      }
+
+    },
+
+    surgeryType: createDependentField({
+      kind: "string",
+      variant: "select",
+      label: "Type of surgery",
+      options: {
+        "Ovariectomy": "Ovariectomy",
+        "Electrode implant": "Electrode implant",
+        "Fiber optic implant": "Fiber optic implant"
+      }
+    },(type) => type === "Surgery"),
+    ovariectomyType: {
+      kind: "dynamic",
+      deps: ["surgeryType"],
+      render(data) {
+        if(data.surgeryType === "Ovariectomy"){
+          return {
+             kind: "string",
+             variant: "select",
+             label: "Ovariectomy sides",
+             options: {
+             "Unilateral":"Unilateral",
+             "Bilateral": "Bilateral"
+             }
+          }
+        }
+        return null
+      }
+    },
+    ovariectomySide: {
+      kind: "dynamic",
+      deps: ["ovariectomyType"],
+      render(data) {
+        if(data.ovariectomyType === "Unilateral"){
+          return {
+             kind: "string",
+             variant: "select",
+             label: "Ovariectomy side",
+             options: {
+             "Right":"Right",
+             "Left": "Left"
+             }
+          }
+        }
+        return null
+      }
+    },
+    ovariectomyGroup: {
+      kind: "dynamic",
+      deps: ["surgeryType"],
+      render(data) {
+        if(data.surgeryType === "Ovariectomy"){
+          return {
+             kind: "string",
+             variant: "select",
+             label: "Ovariectomy group",
+             options: {
+             "Control":"Control",
+             "Experiment": "Experiment"
+             }
+          }
+        }
+        return null
+      }
+    },
+
+    woundDateReported: createDependentField({
+      kind: "date",
+      label: "Wound reported date"
+    }, (type) => type === "Wound treatment"),
+
+    clinicalCondition: createDependentField({
+      kind: "string",
+      variant: "input",
+      label: "Clincal condition"
+    }, (type) => type === "Wound treatment"),
+    
+    treatmentProvided: createDependentField({
+      kind: "string",
+      variant: "input",
+      label: "Treatment provided"
+    }, (type) => type === "Wound treatment"),
+    
+
   },
   details: {
     description: 'Form to be filled in when a mouse goes through a surgery or veterinary care',
@@ -47,9 +182,75 @@ export default defineInstrument({
     license: 'UNLICENSED',
     title: 'Mouse surgery, wound treatment and veterinary care form'
   },
-  measures: {},
+  measures: {
+    treatmentType: {
+      kind: "const",
+      label: "Selected physical intervention",
+      ref: "treatmentType"
+    },
+    aneglesiaUsed: {
+      kind: "const",
+      ref: "aneglesiaUsed"
+    },
+    aneglesiaType: {
+      kind: "const",
+      ref: "aneglesiaType"
+    },
+    aneglesiaVolume: {
+      kind: "const",
+      ref: "aneglesiaVolume"
+    },
+    stereotaxUsed: {
+      kind: "const",
+      ref: "stereotaxUsed"
+    },
+    stereotaxId: {
+      kind: "const",
+      ref: "stereotaxId"
+    },
+    surgeryType: {
+      kind: "const",
+      ref: "surgeryType"
+    },
+    ovariectomyType: {
+      kind: "const",
+      ref: "ovariectomyType"
+    },
+    ovariectomySide: {
+      kind: "const",
+      ref: "ovariectomySide"
+    },
+    ovariectomyGroup: {
+      kind: "const",
+      ref: "ovariectomyGroup"
+    },
+    woundDateReported: {
+      kind: "const",
+      ref: "woundDateReported"
+    },
+    clinicalCondition: {
+      kind: "const",
+      ref: "clinicalCondition"
+    },
+    treatmentProvided: {
+      kind: "const",
+      ref: "treatmentProvided"
+    }
+  },
   validationSchema: z.object({
     treatmentType: z.string(),
-    analglesiaUsed: z.boolean().optional()
+    aneglesiaUsed: z.boolean().optional(),
+    aneglesiaType: z.string().optional(),
+    aneglesiaVolume: z.string().optional(),
+    stereotaxUsed: z.boolean().optional(),
+    stereotaxId: z.string().optional(),
+    surgeryType: z.string().optional(),
+    ovariectomyType: z.string().optional(),
+    ovariectomyGroup: z.string().optional(),
+    ovariectomySide: z.string().optional(),
+    woundDateReported: z.date().optional(),
+    clinicalCondition: z.string().optional(),
+    treatmentProvided: z.string().optional()
+
   })
 });
