1fb62ec27d1fd9946fe4d6b81a9a98f311caf502
David Roper <davidroper321@gmail.com>
2024-08-08 16:13:30 -0400

    additional details to description and instructions

--- lib/forms/CoBrALab-Mouse-Surgery-Form/index.ts
+++ lib/forms/CoBrALab-Mouse-Surgery-Form/index.ts
@@ -176,11 +176,11 @@ export default defineInstrument({
 
   },
   details: {
-    description: 'Form to be filled in when a mouse goes through a surgery or veterinary care',
+    description: 'Describes the information about a physical intervention done upon a Mouse, either in the form of as surgery or as a wound treatment done by the veterinary team.',
     estimatedDuration: 1,
-    instructions: ['Please fill this form whenever a mouse has to through a surgery or has gone through veterinary care'],
+    instructions: ['Please fill this form whenever a mouse has to through a surgery or has gone through veterinary care. If the wound treatment section is selected to be filled in, please use the information written on the card provided by the veterinary team'],
     license: 'UNLICENSED',
-    title: 'Mouse surgery, wound treatment and veterinary care form'
+    title: 'Mouse Surgery and Wound Treatment Form'
   },
   measures: {
     treatmentType: {
