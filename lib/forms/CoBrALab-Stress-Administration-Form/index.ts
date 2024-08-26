be79a4cde964623c452d48ba830fc66bac0864cc
david-roper <davidroper321@gmail.com>
2024-08-26 12:56:49 -0400

    add base form

---
+++ lib/forms/CoBrALab-Stress-Administration-Form/index.ts
@@ -0,0 +1,24 @@
+/* eslint-disable perfectionist/sort-objects */
+
+const { defineInstrument } = await import('/runtime/v1/@opendatacapture/runtime-core/index.js');
+const { z } = await import('/runtime/v1/zod@3.23.x/index.js');
+
+export default defineInstrument({
+  kind: 'FORM',
+  language: 'en',
+  tags: ['<PLACEHOLDER>'],
+  internal: {
+    edition: 1,
+    name: '<PLACEHOLDER>'
+  },
+  content: {},
+  details: {
+    description: '<PLACEHOLDER>',
+    estimatedDuration: 1,
+    instructions: ['<PLACEHOLDER>'],
+    license: 'UNLICENSED',
+    title: '<PLACEHOLDER>'
+  },
+  measures: {},
+  validationSchema: z.object({})
+});
