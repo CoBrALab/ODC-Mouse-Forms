27857e616e0b74a3df93b7d72291b985fbd2eb77
David Roper <60201980+david-roper@users.noreply.github.com>
2024-09-18 13:48:04 -0400

    Mouse origin form (#14)
    * created mouse birth form file
    
    * adding base form
    
    * adding form description and name
    
    * add mother and date of birth to form
    
    * added question about mouses father
    
    * add generation, box, and origin questions
    
    * made mother/father question dependent on box mouse question
    
    * renamed to mouse origin form
    
    * rename folder
    
    * add order id question if its a box mouse
    
    * fixed order id question
    
    * edit box mouse question
    
    * add cohort and sex to form
    
    * adding measures to origin form
    
    * added origin tag and fixed spelling
    
    * add more details to instructions and description
    
    * improved dynamic form logic
    
    * make breeder origin optional question
    
    * added mouse strain question
    
    * added other strain and breeder questions and edits
    
    * added measures for new questions, fixed fatherKnown question
    
    * fixing defineInstrument import
    
    * added limits to generation number question
    
    * removed redundant mouse origin question
    
    * fix generation number integer limit
    
    * fix zod import

---
+++ lib/forms/CoBraLab-Mouse-Origin-Form/index.ts
@@ -0,0 +1,252 @@
+/* eslint-disable perfectionist/sort-objects */
+
+const { defineInstrument } = await import('/runtime/v1/@opendatacapture/runtime-core/index.js');
+const { z } = await import('/runtime/v1/zod@3.23.x/index.js');
+
+export default defineInstrument({
+  kind: 'FORM',
+  language: 'en',
+  tags: ['Birth', 'Mouse','Origin'],
+  internal: {
+    edition: 1,
+    name: 'MOUSE_ORIGIN_FORM'
+  },
+  content: {
+    dateOfBirth: {
+      kind: 'date',
+      label: "date of birth"
+    },
+    mouseSex: {
+      kind: 'string',
+      variant: 'radio',
+      label: 'Sex',
+      options: {
+        "Male": "Male",
+        "Female": "Female"
+      }
+    },
+    cohortId: {
+      kind: 'string',
+      variant: 'input',
+      label: 'Cohort Identification (Optional)'
+    },
+    mouseStrain: {
+      kind: "string",
+      variant: "select",
+      label: "Mouse strain",
+      options: {
+        "M86-hemi": "M86-hemi",
+        "M83-homo": "M83-homo",
+        "C57BL/6J": "C57BL/6J",
+        "Wild type": "Wild type",
+        "5XFAD": "5XFAD",
+        "3xTG-AD": "3xTG-AD",
+        "Other":"Other"
+      }
+    },
+    otherStrain: {
+      kind: "dynamic",
+      deps: ["mouseStrain"],
+      render(data) {
+        if(data.mouseStrain === "Other"){
+           return {
+            kind : "string",
+            variant: "input",
+            label: "Other strain"
+           }
+        }
+        return null
+      }
+    },
+    boxMouse: {
+      kind: 'boolean',
+      variant: 'radio',
+      label: 'Originates from external breeder (Box mouse)?'
+    },
+    orderId: {
+      kind: 'dynamic',
+      deps: ['boxMouse'],
+      render(data) {
+        if (data.boxMouse){
+          return {
+            kind: 'string',
+            variant: 'input',
+            label: 'Order ID'
+          }
+        }
+        return null
+      }
+    },
+    breederOrigin: {
+      kind: 'dynamic',
+      deps: ['boxMouse'],
+      render(data) {
+        if (data.boxMouse){
+          return {
+            kind: 'string',
+            variant: 'select',
+            label: "Origin of breeder",
+            options: {
+              "Charles River Laboratories": "Charles River Laboratories",
+              "Envigo": "Envigo",
+              "Import": "Import",
+              "Jackson Laboratories": "Jackson Laboratories",
+              "Other": "Other"
+            }
+          }
+        }
+        return null
+      }
+    },
+    otherBreederOrigin: {
+      kind: 'dynamic',
+      deps: ["breederOrigin"],
+      render(data) {
+        if(data.breederOrigin === "Other"){
+          return {
+            kind: "string",
+            variant: "input",
+            label: "Other external breeder"
+          }
+        }
+        return null
+      }
+    },
+    motherMouse: {
+      kind: 'dynamic',
+      deps: ['boxMouse'],
+      render(data) {
+       if(data.boxMouse === false){
+         return {
+          kind: "string",
+          variant: "input",
+          label: "Id of mouse's mother"
+         }
+       }
+       return null
+      }
+    },
+    fatherKnown: {
+      kind: 'dynamic',
+      deps: ['boxMouse'],
+      render(data) {
+       if(data.boxMouse === false){
+         return {
+         kind: 'boolean',
+         variant: 'radio',
+         label: "Is the father known?"
+         }
+       }
+       return null
+      }
+      
+    },
+    fatherMouse: {
+     kind: 'dynamic',
+     deps: ['fatherKnown'],
+     render(data) {
+      if(data.boxMouse === false && data.fatherKnown){
+        return {
+          kind: 'string',
+          variant: 'input',
+          label: "Id of mouse's father"
+        }
+      }
+      return null
+     }
+    },
+    generationNumber: {
+      kind: 'number',
+      variant: 'input',
+      label: 'N-generation of mouse'
+    }
+  },
+  details: {
+    description: 'Tracks a mouse\'s birth information, whether it was born within the lab or an exported mouse from an external breeder',
+    estimatedDuration: 1,
+    instructions: ['Whenever a new mouse is born within the lab, or is exported from an external breeder this form should be filled in to log its information. If the mouse is from an external breeder, information of the breeder as well as the box the mouse came in is expected to be known.'],
+    license: 'UNLICENSED',
+    title: 'Mouse Origin Form'
+  },
+  measures: {
+   dateOfBirth: {
+    kind: 'const',
+    label: 'Date of birth',
+    ref: 'dateOfBirth'
+  },
+  mouseSex: {
+    kind: 'const',
+    label: 'Sex',
+    ref: 'mouseSex'
+  },
+  cohortId: {
+    kind: 'const',
+    label: 'Cohort',
+    ref: 'cohortId'
+  },
+  mouseStrain: {
+    kind: 'const',
+    label: 'Mouse Strain',
+    ref: 'mouseStrain'
+  },
+  otherStrain: {
+    kind: 'const',
+    label: 'Other Strain',
+    ref: 'otherStrain'
+  },
+  boxMouse: {
+    kind: 'const',
+    label: 'Imported mouse',
+    ref: "boxMouse"
+  },
+  orderId: {
+    kind: 'const',
+    label: 'Order ID',
+    ref: 'orderId'
+  },
+  breederOrigin: {
+    kind: 'const',
+    label: 'Breeder origin',
+    ref: 'breederOrigin'
+  },
+  otherBreederOrigin: {
+    kind: 'const',
+    label: 'Other Breeder',
+    ref: 'otherBreederOrigin'
+  },
+  motherMouse: {
+    kind: 'const',
+    label: 'Mother of mouse',
+    ref: 'motherMouse'
+  },
+  fatherKnown: {
+    kind: 'const',
+    label: 'Father known',
+    ref: 'fatherKnown'
+  },
+  fatherMouse: {
+    kind: 'const',
+    label: 'Father of mouse',
+    ref: 'fatherMouse'
+  },
+  generationNumber: {
+    kind: 'const',
+    label: 'Generation Number',
+    ref: 'generationNumber'
+  }
+  },
+  validationSchema: z.object({
+    dateOfBirth: z.date(),
+    mouseSex: z.string(),
+    cohortId: z.string().optional(),
+    boxMouse: z.boolean(),
+    mouseStrain: z.string(),
+    otherStrain: z.string().optional(),
+    orderId: z.string().optional(),
+    motherMouse: z.string().optional(),
+    fatherKnown: z.boolean().optional(),
+    fatherMouse: z.string().optional(),
+    breederOrigin: z.string().optional(),
+    otherBreederOrigin: z.string().optional(),
+    generationNumber: z.number().min(1).int()})
+});
