8eb3ae8f35822a10646a91f124b7e69b37c8781a
David Roper <60201980+david-roper@users.noreply.github.com>
2024-10-02 14:24:22 -0400

    Mouse weight form (#12)
    * adding default for  mouse weight form
    
    * added weight question, allows float values
    
    * added mouse weight range check
    
    * add scale used question
    
    * fixed form internal name
    
    * update readme with POST request commands
    
    * POST form request files
    
    * removed unneeded post request code
    
    * changed scale used to scale identification
    
    * change identification to id
    
    * add kind of scale question
    
    * remove float workaround
    
    * fixing defineInstrument import
    
    * removing redundant info from readme
    
    * remove blank space
    
    * change identification to serial code
    
    * fix zod import
    
    * add serial code as select field, with other revealing an input field
    
    * add proper name to regular scale and edit weight limit

---
+++ lib/forms/CoBraLab-Mouse-Weight-Form/index.ts
@@ -0,0 +1,89 @@
+/* eslint-disable perfectionist/sort-objects */
+
+const { defineInstrument } = await import('/runtime/v1/@opendatacapture/runtime-core/index.js');
+const { z } = await import('/runtime/v1/zod@3.23.x/index.js');
+
+export default defineInstrument({
+  kind: 'FORM',
+  language: 'en',
+  tags: ['Mouse', 'Weight', 'Scale'],
+  internal: {
+    edition: 1,
+    name: 'MOUSE_WEIGHT_FORM'
+  },
+  content: {
+    mouseWeight: {
+      kind: 'number',
+      variant: 'input',
+      label: 'Weight of mouse (in grams)'
+    },
+    scaleSerialCode: {
+      kind: 'string',
+      variant: 'select',
+      label: "Scale serial code",
+      options: {
+        "8334378068":"8334378068",
+        "Other": "Other"
+      }
+    },
+    scaleSerialCodeOther: {
+      kind: "dynamic",
+      deps: ["scaleSerialCode"],
+      render(data) {
+        if(data.scaleSerialCode === "Other"){
+          return {
+            kind: "string",
+            variant: "input",
+            label: "Please enter other serial code"
+          }
+        }
+        return null
+      }
+      
+    },
+    scaleKind: {
+      kind: 'string',
+      variant: 'select',
+      label: "Kind of scale",
+      options: {
+        "Portable": "Small portable",
+        "Regular": "Regular (Scout Pro SP401)"
+      }
+    }
+  },
+  details: {
+    description: 'A form to track data from whenever an animal is weighed.',
+    estimatedDuration: 1,
+    instructions: ['To be filled in whenever the animal is weighed. It is expected to know what type of scale is used (portable vs. non-portable) as well as its serial code. It is also assumed that proper weighing protocol is followed'],
+    license: 'UNLICENSED',
+    title: 'Mouse Weight Form'
+  },
+  measures: {
+    mouseWeight: {
+      kind: 'const',
+      label: 'Mouse weight',
+      ref: 'mouseWeight'
+    },
+    scaleSerialCode: {
+      kind: 'const',
+      label: "Scale serial code",
+      ref: "scaleSerialCode"
+    },
+    scaleSerialCodeOther: {
+      kind: 'const',
+      label: "Other Scale serial code provided",
+      ref: "scaleSerialCode"
+    },
+    scaleKind: {
+      kind: "const",
+      ref: "scaleKind"
+    }
+
+  },
+  validationSchema: z.object({
+    mouseWeight: z.number().min(1).max(100),
+    scaleSerialCode: z.string(),
+    scaleSerialCodeOther: z.string().optional(),
+    scaleKind: z.string()
+  })
+});
